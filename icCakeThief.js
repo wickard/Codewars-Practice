694a8089945cbf5522e1f20650c58e958acc7a75
Josh <carlson.joshuaph@gmail.com>
2018-01-31 07:12:31 -0600

    first solution to IC problem cakethief

---
+++ icCakeThief.js
@@ -0,0 +1,43 @@
+// Write a function maxDuffelBagValue() that takes an array of cake type objects and a weight capacity, and returns the maximum monetary value the duffel bag can hold.
+
+// For example:
+
+//   var cakeTypes = [
+//     {weight: 7, value: 160},
+//     {weight: 3, value: 90},
+//     {weight: 2, value: 15},
+// ];
+
+// var capacity = 20;
+
+// maxDuffelBagValue(cakeTypes, capacity);
+// // returns 555 (6 of the middle type of cake and 1 of the last type of cake)
+
+const maxDuffelBagValue = (cakeTypes, capacity) => {
+  let totalValue = 0;
+  if (!capacity){
+    return 0;
+  }
+  for (let i in cakeTypes){
+    if (cakeTypes[i].weight === 0){
+      return Infinity;
+    }
+    cakeTypes[i].unitVal = cakeTypes[i].value / cakeTypes[i].weight
+  }
+  cakeTypes.sort((a,b) => b.unitVal - a.unitVal);
+  for (let i = 0; capacity > 0 && i < cakeTypes.length; i++){
+    let cake = cakeTypes[i]
+    let numCakes = Math.floor(capacity / cake.weight)
+    totalValue += cake.value * numCakes
+    capacity -= cake.weight * numCakes
+  }
+  return totalValue
+}
+
+  var cakeTypes = [
+    {weight: 7, value: 160},
+    {weight: 3, value: 90},
+    {weight: 2, value: 15},
+];
+
+console.log(maxDuffelBagValue(cakeTypes, 20))
